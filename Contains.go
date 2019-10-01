a8098e569bfb46a3e68f1ae475e80bac875c6bbd
Mehmet Emin Kaymaz <mehemteminkaymaz24@gmail.com>
2019-10-01 18:15:56 +0300

    comments

--- Contains.go
+++ Contains.go
@@ -3,18 +3,18 @@ package koala
 import "reflect"
 
 
-//return a boolean value , if exist true else false
+//Contains returns a boolean value , if exist true else false
 //its also variadic function
 func(c Collection) Contains(item ...interface{})Collection{
 
-	t:=Task{
-		KindOfTask:"Void",
-		Do: func() interface{} {
+	t:=Task{ //create task
+		KindOfTask:"Void", //type of task
+		Do: func() interface{} { //the function return the task
 			return func(col *Collection,source interface{}){
 
 				slicev:=reflect.ValueOf(source)
 				var state =false
-				for i:=0;i<slicev.Len();i++{
+				for i:=0;i<slicev.Len();i++{ //loop over slice
 					for k:=0; k<col.Source.Len();k++{
 						if col.Source.Index(k).Interface()==slicev.Index(i).Interface(){
 							state=true
@@ -23,13 +23,13 @@ func(c Collection) Contains(item ...interface{})Collection{
 						}
 					}
 				}
-				col.Source=reflect.ValueOf(state)
+				col.Source=reflect.ValueOf(state)//change source
 			}
 		},
 	}
 
-	c.Tasks=append(c.Tasks,t)
-	c.Items=append(c.Items,item)
+	c.Tasks=append(c.Tasks,t) //update collection's tasks
+	c.Items=append(c.Items,item) //update collection's items
 
-	return c
+	return c //return the collection
 }
